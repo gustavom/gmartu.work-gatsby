09e97e121e6420b030c27a0e060f013a1b00dc1b
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2020-09-13 00:39:47 -0300

    install some plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,5 +7,53 @@ const siteMetadata = require("./config/metadata")
 module.exports = {
   /* Your site config here */
   siteMetadata,
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Gustavo Martusewicz`,
+        short_name: `gmartu.work`,
+        description: `Creating great user experiencies - Front-End Developer`,
+        lang: `pt-br`,
+        start_url: `/`,
+        background_color: `#222222`,
+        theme_color: `#222222`,
+        display: `standalone`,
+        icon: `${__dirname}/static/images/favicon.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-77333673-2",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        // Setting a color is optional.
+        color: `#222222`,
+        // Disable the loading spinner.
+        showSpinner: false,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        displayName: process.env.NODE_ENV !== "production",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `IBM Plex Sans`,
+            subsets: [`400`, `500`, `600`, `700`],
+          },
+        ],
+      },
+    }`gatsby-plugin-offline`,
+  ],
 }
