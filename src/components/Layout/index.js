8cbf107c130a2a6801f028281f1ae99e99767b55
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2020-09-16 18:08:12 -0300

    add global style

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -4,9 +4,12 @@ import { Main } from "./styles"
 
 import Header from "../Header"
 
+import GlobalStyle from "../global-styles"
+
 function Layout({ children }) {
   return (
     <Main>
+      <GlobalStyle />
       <Header />
       {children}
     </Main>
---
+++ src/components/global-styles.js
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from "styled-components"
+
+export default createGlobalStyle`
+  body {
+    font-family: "IBM Plex Sans";
+    font-weight: 400;
+  }
+`
