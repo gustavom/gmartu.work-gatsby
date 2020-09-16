a5b8060cee57c017a2075b546730a1aa96ae6298
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2020-09-16 15:02:38 -0300

    adjust config to multiple content types

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,7 +67,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/blog/`,
-        name: `blog`,
+        name: `post`,
       },
     },
     {
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,10 @@
 // post guide https://www.gatsbyjs.com/tutorial/part-seven/5
+// https://desktopofsamuel.com/building-gatsby-with-multiple-post-type
 const path = require(`path`)
 const { createFilePath } = require("gatsby-source-filesystem")
+
+const blogPost = path.resolve(`./src/templates/blog-post.js`)
+const projectPage = path.resolve(`./src/templates/project-page.js`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
@@ -26,6 +30,9 @@ exports.createPages = async ({ graphql, actions }) => {
             fields {
               slug
             }
+            frontmatter {
+              categories
+            }
           }
         }
       }
@@ -33,14 +40,27 @@ exports.createPages = async ({ graphql, actions }) => {
   `)
   console.log(JSON.stringify(result, null, 4))
   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-    createPage({
-      path: node.fields.slug,
-      component: path.resolve(`./src/templates/blog-post.js`),
-      context: {
-        // Data passed to context is available
-        // in page queries as GraphQL variables.
-        slug: node.fields.slug,
-      },
-    })
+    console.log(node.frontmatter.categories[0])
+    if (node.frontmatter.categories[0] === "Projetos") {
+      createPage({
+        path: node.fields.slug,
+        component: projectPage,
+        context: {
+          // Data passed to context is available
+          // in page queries as GraphQL variables.
+          slug: node.fields.slug,
+        },
+      })
+    } else {
+      createPage({
+        path: node.fields.slug,
+        component: blogPost,
+        context: {
+          // Data passed to context is available
+          // in page queries as GraphQL variables.
+          slug: node.fields.slug,
+        },
+      })
+    }
   })
 }
