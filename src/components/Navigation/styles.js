ebd2d0e9c465d1d444c99cee7af87a26898d3768
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2020-09-16 22:46:36 -0300

    style nav

--- src/components/Navigation/styles.js
+++ src/components/Navigation/styles.js
@@ -1,3 +1,22 @@
 import styled from "styled-components"
 
-export const Container = styled.nav``
+export const Container = styled.nav`
+  border-bottom: solid 1px #222;
+  ul {
+    list-style: none;
+    display: flex;
+    li {
+      flex: 0 0 33.33%;
+      padding: 1rem;
+      font-size: 1.4rem;
+      font-weight: 500;
+      &:not(:last-child) {
+        border-right: solid 1px #222;
+      }
+      a {
+        color: #222;
+        text-decoration: none;
+      }
+    }
+  }
+`
