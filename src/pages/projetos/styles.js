d488370ecf95b91859d902090f2422aac794531a
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2020-12-07 22:51:00 -0300

    add locomotive and hover image

--- src/pages/projetos/styles.js
+++ src/pages/projetos/styles.js
@@ -6,7 +6,35 @@ export const Container = styled.section`
 
 export const ProjectList = styled.div``
 
-export const ProjectItem = styled.article``
+export const ProjectItem = styled.article`
+  width: 100%;
+  /* background-color: rgb(0, 0, 0, 0.1); */
+  border-bottom: solid 4px black;
+  font-weight: 500;
+  font-size: 4vw;
+  margin-bottom: 50px;
+  a {
+    text-decoration: none;
+    color: black;
+  }
+`
 
-export const ProjectItemImage = styled.div``
+export const ProjectItemImage = styled.div`
+  display: none;
+  ${ProjectItem}:hover & {
+    display: block;
+    position: fixed;
+    width: 40%;
+    height: 100vh;
+    top: 0;
+    right: 0;
+    z-index: 10;
+    overflow: hidden;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+`
 export const ProjectItemTitle = styled.div``
