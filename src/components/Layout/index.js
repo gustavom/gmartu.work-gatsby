c76415f0d65e6aee8e2bc1177673add506e14bc5
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2020-12-24 02:42:24 -0300

    add locomotive scroll lib

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -7,12 +7,24 @@ import Header from "../Header"
 import GlobalStyle from "../global-styles"
 // import { AnimatePresence, motion } from "framer-motion"
 
+import locomotiveScroll from "locomotive-scroll"
+
 function Layout({ children }) {
   useEffect(() => {
     console.log("mount")
   }, [])
+
+  const scrollRef = React.createRef()
+
+  useEffect(() => {
+    const scroll = new locomotiveScroll({
+      el: scrollRef.current,
+      smooth: true,
+    })
+  })
+
   return (
-    <Main>
+    <Main ref={scrollRef}>
       <GlobalStyle />
       <Header />
       <>{children}</>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,19 @@
-import React from "react"
-// import { graphql } from "gatsby"
+import React, { useEffect } from "react"
+import { graphql, Link } from "gatsby"
 import Layout from "../components/Layout"
 import { Container, HomeContainer } from "./styles"
 
 import SEO from "../components/SEO"
+import {
+  ProjectItem,
+  ProjectItemImage,
+  ProjectItemTitle,
+  ProjectList,
+} from "./projetos/styles"
 
-export default function Home() {
+export default function Home({ data }) {
+  const project = data.portfolio.edges
+
   return (
     <Layout>
       <SEO />
@@ -15,6 +23,56 @@ export default function Home() {
           <span>Developer</span>
         </h1>
       </HomeContainer>
+      <ProjectList>
+        {project.map(({ node }) => (
+          <ProjectItem key={node.fields.slug}>
+            <Link to={node.fields.slug}>
+              <ProjectItemImage>
+                <img
+                  src={node.frontmatter.image}
+                  alt={node.frontmatter.title}
+                />
+              </ProjectItemImage>
+              <ProjectItemTitle>{node.frontmatter.title}</ProjectItemTitle>
+            </Link>
+          </ProjectItem>
+        ))}
+      </ProjectList>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    portfolio: allMarkdownRemark(
+      filter: { frontmatter: { categories: { eq: "Projetos" } } }
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            image
+          }
+        }
+      }
+    }
+    # labs: allMarkdownRemark(
+    #   filter: { frontmatter: { categories: { eq: "labs" } } }
+    # ) {
+    #   edges {
+    #     node {
+    #       fields {
+    #         slug
+    #       }
+    #       frontmatter {
+    #         title
+    #         image
+    #       }
+    #     }
+    #   }
+    # }
+  }
+`
