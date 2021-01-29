ba6431771f07e1e775952f09c20a5314756e775c
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2021-01-29 15:30:51 -0300

    clean and add height: 100vh;

--- src/pages/projetos/styles.js
+++ src/pages/projetos/styles.js
@@ -1,5 +1,6 @@
 import styled from "styled-components"
 
 export const Container = styled.section`
+  height: 100vh;
   padding: 4.5rem 1.6rem 1.6rem;
 `
--- src/pages/sobre/styles.js
+++ src/pages/sobre/styles.js
@@ -1,6 +1,7 @@
 import styled from "styled-components"
 
 export const Container = styled.section`
+  height: 100vh;
   padding: 4.5rem 1.6rem 1.6rem;
   p {
     font-size: 1.6rem;
