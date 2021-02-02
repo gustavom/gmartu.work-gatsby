81d09adc87191bd8d2e0f7cdcbfa1c77db164560
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2021-02-02 03:02:18 -0300

    about page details and side bar nav

--- src/pages/sobre/index.js
+++ src/pages/sobre/index.js
@@ -1,37 +1,62 @@
+import { Link } from "gatsby"
 import React from "react"
 
 import Layout from "../../components/Layout"
 
-import { Container } from "./styles"
+import { ColLeft, ColRight, Container } from "./styles"
 
 export default function Sobre() {
   return (
     <Layout>
       <Container>
-        <h1>Sobre</h1>
-        <p>
-          Olá, sou desenvolvedor front-end, atualmente trabalho na PUC-SP, uma
-          das universidades mais importantes do Brasil.
-        </p>
-        <p>
-          Tenho mais de 10 anos de experiência na área, desde webdesigner,
-          garoto do html + css , cortador de layout e outros nomes que se
-          perderam no tempo.
-        </p>
-        <p>
-          Atualmente trabalho na PUC-SP, uma das universidades mais importantes
-          do Brasil. Lá trabalhamos na sustentação do portal da Universidade,
-          que criamos com Drupal 8, twig, sass, gulp e na criação e manutenção
-          de outros projetos que ficam sob o "guarda-chuva" da universidade.
-        </p>
-        <p>
-          Eu criei este site pessoal para mostrar um pouco dos projetos que
-          participei ao mundo e também para treinar (já fiz uma versão com
-          gerenciador de conteúdo com NodeJs, Express, Sequelize e Postgres e o
-          front-end feito com ReactJS, se me perguntar, podemos falar mais sobre
-          isso), e essa atual esta feita com GatsbyJS e Markdown para os
-          projetos e para talvez, um blog.
-        </p>
+        <ColLeft>
+          <h1>Sobre</h1>
+          <p>
+            Olá, sou desenvolvedor front-end, atualmente trabalho na
+            universidade PUC-SP.
+          </p>
+          <p>
+            Tenho mais de 10 anos de experiência na área, desde webdesigner,
+            garoto do html + css , cortador de layout e outros nomes que se
+            perderam no tempo.
+          </p>
+          <p>
+            Atualmente, na PUC-SP, trabalhamos na sustentação do portal da
+            universidade, que criamos com Drupal 8, twig, sass, gulp e na
+            criação e manutenção de outros projetos que ficam sob o
+            "guarda-chuva" da universidade.
+          </p>
+          <p>
+            Eu criei este site pessoal para mostrar um pouco dos projetos que
+            participei ao mundo e também para treinar (já fiz uma versão com
+            gerenciador de conteúdo com NodeJs, Express, Sequelize e Postgres e
+            o front-end feito com ReactJS, se me perguntar, podemos falar mais
+            sobre isso), e essa atual esta feita com GatsbyJS e Markdown para os
+            projetos e para talvez, um blog.
+          </p>
+        </ColLeft>
+        <ColRight>
+          <ul>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/gustavo-martusewicz/"
+                target="_blank"
+              >
+                Linkedin
+              </a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/dev_gustavom/" target="_blank">
+                Instagram
+              </a>
+            </li>
+            <li>
+              <a href="mailto:gustavo_crj@hotmail.com" target="_blank">
+                Email
+              </a>
+            </li>
+          </ul>
+        </ColRight>
       </Container>
     </Layout>
   )
--- src/pages/sobre/styles.js
+++ src/pages/sobre/styles.js
@@ -1,8 +1,9 @@
 import styled from "styled-components"
 
 export const Container = styled.section`
+  display: flex;
   height: 100vh;
-  padding: 7.5rem 1.6rem 1.6rem;
+  padding: 7.5rem 4rem 1.6rem;
   /* padding: 4.5rem 1.6rem 1.6rem; */
   h1 {
     font-size: 50px;
@@ -12,6 +13,32 @@ export const Container = styled.section`
     font-size: 1.6rem;
     & + p {
       padding-top: 30px;
+    }
+  }
+`
+export const ColLeft = styled.div`
+  flex: 0 0 70%;
+  max-width: 70%;
+  order: 2;
+`
+export const ColRight = styled.div`
+  flex: 0 0 30%;
+  max-width: 30%;
+  order: 1;
+  padding-top: 77px;
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    li {
+      font-size: 20px;
+      a {
+        color: #000;
+        text-decoration: none;
+      }
+      & + li {
+        margin-top: 15px;
+      }
     }
   }
 `
