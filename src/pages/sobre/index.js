fa4311fb89536b818baa1101202ea01488de7a32
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2021-02-02 02:37:58 -0300

    adjust text and styles

--- src/pages/sobre/index.js
+++ src/pages/sobre/index.js
@@ -8,15 +8,15 @@ export default function Sobre() {
   return (
     <Layout>
       <Container>
-        <h1>sobre</h1>
+        <h1>Sobre</h1>
         <p>
-          Olá, me chamo Gustavo Martusewicz e trabalho atualmente como
-          desenvolvedor front-end.
+          Olá, sou desenvolvedor front-end, atualmente trabalho na PUC-SP, uma
+          das universidades mais importantes do Brasil.
         </p>
         <p>
-          Tenho mais de 10 anos de experiência na área de desenvolvimento web,
-          desde webdesigner, garoto do html + css , cortador de layout e outros
-          nomes que se perderam no tempo.
+          Tenho mais de 10 anos de experiência na área, desde webdesigner,
+          garoto do html + css , cortador de layout e outros nomes que se
+          perderam no tempo.
         </p>
         <p>
           Atualmente trabalho na PUC-SP, uma das universidades mais importantes
@@ -32,8 +32,6 @@ export default function Sobre() {
           isso), e essa atual esta feita com GatsbyJS e Markdown para os
           projetos e para talvez, um blog.
         </p>
-        <p>Sinta-se livre para navegar, olhar e avaliar!</p>
-        <p>Obrigado!</p>
       </Container>
     </Layout>
   )
--- src/pages/sobre/styles.js
+++ src/pages/sobre/styles.js
@@ -2,8 +2,16 @@ import styled from "styled-components"
 
 export const Container = styled.section`
   height: 100vh;
-  padding: 4.5rem 1.6rem 1.6rem;
+  padding: 7.5rem 1.6rem 1.6rem;
+  /* padding: 4.5rem 1.6rem 1.6rem; */
+  h1 {
+    font-size: 50px;
+    font-weight: 600;
+  }
   p {
     font-size: 1.6rem;
+    & + p {
+      padding-top: 30px;
+    }
   }
 `
