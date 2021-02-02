1f883c9dd6021af20bbc54a2d8a1d012deac4b42
Gustavo Martusewicz <gustavo_crj@hotmail.com>
2021-02-02 02:38:17 -0300

    add styles

--- src/pages/projetos/styles.js
+++ src/pages/projetos/styles.js
@@ -2,5 +2,14 @@ import styled from "styled-components"
 
 export const Container = styled.section`
   height: 100vh;
-  padding: 4.5rem 1.6rem 1.6rem;
+  padding: 7.5rem 1.6rem 1.6rem;
+  /* padding: 4.5rem 1.6rem 1.6rem; */
+  h1 {
+    font-size: 50px;
+    font-weight: 600;
+  }
+
+  #project-list {
+    padding: 30px 5px;
+  }
 `
